be6c7f22094d42eb221b34ec43a55a985277bec8
su-ex <codeworks@supercable.onl>
2021-07-05 20:29:53 +0200

    Apply .sc_BubbleLayout tp ForwardDialog preview

--- src/components/views/dialogs/ForwardDialog.tsx
+++ src/components/views/dialogs/ForwardDialog.tsx
@@ -221,6 +221,7 @@ const ForwardDialog: React.FC<IProps> = ({ matrixClient: cli, event, permalinkCr
         <div className={classnames("mx_ForwardDialog_preview", {
             "mx_IRCLayout": previewLayout == Layout.IRC,
             "mx_GroupLayout": previewLayout == Layout.Group,
+            "sc_BubbleLayout": previewLayout == Layout.Bubble,
         })}>
             <EventTile
                 mxEvent={mockEvent}
