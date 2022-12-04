23de404c637747b5f2662b12355b0cbc4ab0d951
su-ex <codeworks@supercable.onl>
2022-12-04 02:55:02 +0100

    Fix send button shown when not being appropriate

--- src/components/views/rooms/MessageComposer.tsx
+++ src/components/views/rooms/MessageComposer.tsx
@@ -560,7 +560,7 @@ export class MessageComposer extends React.Component<IProps, IState> {
             />,
         );
 
-        const showSendButton = true || !this.state.isComposerEmpty || this.state.haveRecording;
+        const showSendButton = canSendMessages || !this.state.isComposerEmpty || this.state.haveRecording;
 
         const classes = classNames({
             "mx_MessageComposer": true,
