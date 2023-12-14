59ab8ce8f5250b52b5ed0a5e2269b800b6697d8b
SpiritCroc <dev@spiritcroc.de>
2023-12-14 10:32:10 +0100

    Don't preview reactions

--- src/stores/room-list/MessagePreviewStore.ts
+++ src/stores/room-list/MessagePreviewStore.ts
@@ -68,10 +68,12 @@ const PREVIEWS: Record<
         isState: false,
         previewer: new StickerEventPreview(),
     },
+    /*
     "m.reaction": {
         isState: false,
         previewer: new ReactionEventPreview(),
     },
+    */
     [M_POLL_START.name]: {
         isState: false,
         previewer: new PollStartEventPreview(),
